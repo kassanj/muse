8848c4e808ffe3f4118a7f8802b7c765d5d8a785
Kassandra M <kassandram022@gmail.com>
2015-04-26 09:14:53 -0400

    finish setup for Dropbox

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,15 @@
 class Post < ActiveRecord::Base
+
   acts_as_votable
   belongs_to :user
   has_many :comments
+if Rails.env.development?
+  has_attached_file :image, :styles => { medium: "700x500#", small: "350x250#"},
+else
   has_attached_file :image, :styles => { medium: "700x500#", small: "350x250#"},
             :storage => :dropbox,
             :dropbox_credentials => Rails.root.join("config/dropbox.yml")
             validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+            :path => ":style/:id_:filename"
+end
 end
