c5f2963bd392ee92c3f7175ca09b9a4d7f779789
Kassandra M <kassandram022@gmail.com>
2015-04-25 23:33:08 -0400

    add figaro gem

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,8 +2,8 @@ class Post < ActiveRecord::Base
   acts_as_votable
   belongs_to :user
   has_many :comments
-  has_attached_file :image, :styles => { medium: "700x500#", small: "350x250#"}
-  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/,
+  has_attached_file :image, :styles => { medium: "700x500#", small: "350x250#"},
             :storage => :dropbox,
             :dropbox_credentials => Rails.root.join("config/dropbox.yml")
+            validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
