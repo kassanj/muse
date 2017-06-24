d47a7936f0256720e9995d9a17c0b66968ef021d
Kassandra Meyer <kassandra@whereby.us>
2017-06-24 14:05:37 -0400

    fix paperclip error

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,15 +1,17 @@
 class Post < ActiveRecord::Base
 
-  acts_as_votable
-  belongs_to :user
-  has_many :comments
-if Rails.env.development?
-  has_attached_file :image, :styles => { medium: "700x500#", small: "350x250#"},
-else
-  has_attached_file :image, :styles => { medium: "700x500#", small: "350x250#"},
-            :storage => :dropbox,
-            :dropbox_credentials => Rails.root.join("config/dropbox.yml")
-            validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
-            :path => ":style/:id_:filename"
-end
+	acts_as_votable
+	belongs_to :user
+	has_many :comments
+
+	if Rails.env.development?
+	  has_attached_file :image, :styles => { medium: "700x500#", small: "350x250#"}
+	  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif", "application/pdf"]
+	else
+	  has_attached_file :image, :styles => { medium: "700x500#", small: "350x250#"},
+	            :storage => :dropbox,
+	            :dropbox_credentials => Rails.root.join("config/dropbox.yml"), 
+	            :path => ":style/:id_:filename"
+	  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif", "application/pdf"]
+	end
 end
